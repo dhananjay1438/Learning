0c7bbf364d890f89973915ac2d1749e1d9a00665
Dhananjay Panage <dhananjaypanage11@gmail.com>
2021-05-04 16:06:59 +0530

    Removed unnecessary array which used to hold function pointers

--- calling_function_based_on_input.cpp
+++ calling_function_based_on_input.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <vector>
 
 class divide_by_zero : public std::exception {
 public:
@@ -20,41 +21,45 @@ public:
     return "This is not how this is intented to use";
   }
 };
-inline void print_usage() { std::cout << "Usage ./a.out a [+|-|/|%] b"; }
+inline void print_usage() { std::cout << "Usage ./a.out a [+|-|x|/|%] b"; }
 inline int add(int a, int b) { return a + b; }
+inline int mul(int a, int b) { return a * b; }
 inline int sub(int a, int b) { return a - b; }
 inline int divide(int a, int b) { return a / b; }
 inline int mod(int a, int b) { return a % b; }
 
 int main(int argc, char *argv[]) {
+  std::vector<std::string> args(argv + 1, argv + argc);
   try {
     if (argc != 4) {
       throw no_proper_usage();
     }
+
+    for (auto x : args) {
+      std::cout << x << " ";
+    }
+    std::cout << "= ";
     typedef int (*Handler)(int, int);
 
-    Handler functions[]{add, sub, divide, mod};
-
-    std::map<char, int> operators;
-    operators['+'] = 0;
-    operators['-'] = 1;
-    operators['/'] = 2;
-    operators['%'] = 3;
+    std::map<char, Handler> operations;
+    operations.insert(std::pair<char, Handler>('+', add));
+    operations.insert(std::pair<char, Handler>('-', sub));
+    operations.insert(std::pair<char, Handler>('x', mul));
+    operations.insert(std::pair<char, Handler>('/', divide));
+    operations.insert(std::pair<char, Handler>('%', mod));
 
     int n1 = std::stoi(argv[1]);
     int n2 = std::stoi(argv[3]);
     char op = argv[2][0];
 
-    auto it = operators.find(op);
-
-    if (n2 == 0 && it->second == '/') {
+    if (n2 == 0 && op == '/') {
       throw divide_by_zero();
     }
 
-    if (it == operators.end()) {
+    if (operations.find(op) == operations.end()) {
       throw no_such_operator();
     }
-    std::cout << functions[it->second](n1, n2);
+    std::cout << operations[op](n1, n2);
 
   } catch (no_such_operator &obj) {
 
@@ -68,17 +73,17 @@ int main(int argc, char *argv[]) {
 
   } catch (std::invalid_argument &obj) {
 
-    std::cout << "Please enter integers only";
+    std::cout << "Please enter integers only" << std::endl;
     print_usage();
 
   } catch (divide_by_zero &obj) {
 
-    std::cout << obj.what();
+    std::cout << obj.what() << std::endl;
     print_usage();
 
   } catch (...) {
 
-    std::cout << "Something happend!";
+    std::cout << "Something happend!" << std::endl;
     print_usage();
   }
 
